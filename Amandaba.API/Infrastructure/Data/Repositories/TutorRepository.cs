43f5e453ea4a8dd236fa3bf47f2bf6798b31c936
grazialencar <graziellealencar3@gmail.com>
2026-09-13 23:09:25 -0300

    Simplifica consultas do TutorRepository para evitar erro do provider Oracle com Include

--- Amandaba.API/Infrastructure/Data/Repositories/TutorRepository.cs
+++ Amandaba.API/Infrastructure/Data/Repositories/TutorRepository.cs
@@ -1,6 +1,5 @@
 using Amandaba.API.Domain.Interfaces;
 using Amandaba.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace Amandaba.Infrastructure.Data.Repositories
 {
@@ -15,44 +14,35 @@ namespace Amandaba.Infrastructure.Data.Repositories
 
 		public TutorEntity? ObterPorEmail(string email)
 		{
-			var tutor = _context.Tutores
-				.Include(t => t.Usuario)
-				.FirstOrDefault(t => t.Usuario.Email == email);
-
-			if (tutor is not null)
-				return tutor;
-
 			var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == email);
 			if (usuario is null)
 				return null;
 
-			return CriarTutorParaUsuario(usuario);
+			return ObterOuCriarTutor(usuario);
 		}
 
 		public TutorEntity? ObterPorIdUsuario(decimal idUsuario)
 		{
-			var tutor = _context.Tutores
-				.Include(t => t.Usuario)
-				.FirstOrDefault(t => t.IdUsuario == idUsuario);
-
-			if (tutor is not null)
-				return tutor;
-
 			var usuario = _context.Usuarios.Find(idUsuario);
 			if (usuario is null)
 				return null;
 
-			return CriarTutorParaUsuario(usuario);
+			return ObterOuCriarTutor(usuario);
 		}
 
-		private TutorEntity CriarTutorParaUsuario(UsuarioEntity usuario)
+		private TutorEntity ObterOuCriarTutor(UsuarioEntity usuario)
 		{
-			var novoTutor = new TutorEntity { IdUsuario = usuario.IdUsuario };
-			_context.Tutores.Add(novoTutor);
-			_context.SaveChanges();
+			var tutor = _context.Tutores.FirstOrDefault(t => t.IdUsuario == usuario.IdUsuario);
 
-			novoTutor.Usuario = usuario;
-			return novoTutor;
+			if (tutor is null)
+			{
+				tutor = new TutorEntity { IdUsuario = usuario.IdUsuario };
+				_context.Tutores.Add(tutor);
+				_context.SaveChanges();
+			}
+
+			tutor.Usuario = usuario;
+			return tutor;
 		}
 	}
 }
